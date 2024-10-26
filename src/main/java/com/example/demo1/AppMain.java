46bc23874abc670af4d86f83db970f234c1b6ef3
David <davidoliveira3537@gmail.com>
2024-10-25 21:01:40 -0300

    segundo commit

--- src/main/java/com/example/demo1/AppMain.java
+++ src/main/java/com/example/demo1/AppMain.java
@@ -2,16 +2,20 @@ package com.example.demo1;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class AppMain extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        String pathMain="C:\\Users\\Unifan\\Documents\\demo1\\src\\main\\java\\com\\example\\demo1\\View\\MainView.fxml";
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        Parent  root = fxmlLoader.load(new FileInputStream(pathMain));
+        Scene scene = new Scene(root, 800, 550);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -13,4 +13,6 @@ module com.example.demo1 {
 
     opens com.example.demo1 to javafx.fxml;
     exports com.example.demo1;
+    exports com.example.demo1.Controller;
+    opens com.example.demo1.Controller to javafx.fxml;
 }
